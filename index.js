1c9ed0edc1183c6365ace3e9d0c1b0f0d5729a75
NataliiaRybalka <natalirybalka21@gmail.com>
2024-09-16 15:30:34 +0300

    add mergePDFs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import fs from 'fs';
 import sharp from 'sharp';
 import { PDFDocument } from 'pdf-lib';
 
-async function createPDFWithImages(imagePaths, outputPDFPath) {
+const imagePaths = [];
+
+async function createPDFWithImages(imagePaths, outputSmallPDFPath) {
 	const pdfDoc = await PDFDocument.create();
 
 	for (const imagePath of imagePaths) {
@@ -32,13 +34,38 @@ async function createPDFWithImages(imagePaths, outputPDFPath) {
 	
 	const pdfBytes = await pdfDoc.save();
 	
-	fs.writeFileSync(outputPDFPath, pdfBytes);
-	console.log(`PDF created successfully at: ${outputPDFPath}`);
+	fs.writeFileSync(outputSmallPDFPath, pdfBytes);
+	console.log(`PDF created successfully at: ${outputSmallPDFPath}`);
 }
 
-const imagePaths = ['image1.jpg', 'image2.png', 'image3.jpg'];
+async function saveImagesPathes(folderName) {
+	const files = fs.readdirSync(`../${folderName}`);
+	files.forEach(file => imagePaths.push(`../${folderName}/${file}`));
+
+	createPDFWithImages(imagePaths, `${folderName}.pdf`);
+}
+
+saveImagesPathes('01');
+
+const pdfPaths = [];
 const outputPDFPath = 'planner.pdf';
 
-createPDFWithImages(imagePaths, outputPDFPath)
-	.then(() => console.log('PDF generation complete'))
-	.catch((err) => console.error('Error generating PDF:', err));
+async function mergePDFs(pdfPaths, outputPDFPath) {
+	const mergedPdf = await PDFDocument.create();
+
+	for (const pdfPath of pdfPaths) {
+		const pdfBytes = fs.readFileSync(pdfPath);
+		const pdfDoc = await PDFDocument.load(pdfBytes);
+		const copiedPages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
+
+		copiedPages.forEach((page) => mergedPdf.addPage(page));
+	}
+
+	const mergedPdfBytes = await mergedPdf.save();
+
+	fs.writeFileSync(outputPDFPath, mergedPdfBytes);
+
+	console.log(`Merged PDF saved to: ${outputPDFPath}`);
+}
+
+// mergePDFs(pdfPaths, 'planner.pdf');
